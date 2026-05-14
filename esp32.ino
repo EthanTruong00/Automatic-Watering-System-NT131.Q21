b78d4541ab3ded330e781404d250fabc5c69ecec
EthanTruong00 <ethancoding1310@gmail.com>
2026-05-15 02:30:24 +0700

    Xây dựng hàm điều khiển Relay và thuật toán xả áp suất chống vỡ ống

--- esp32.ino
+++ esp32.ino
@@ -1,124 +1,117 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPL6SHeWCWKU"
+#define BLYNK_TEMPLATE_ID   "TMPL6SHeWCWKU"
 #define BLYNK_TEMPLATE_NAME "Smart Watering System"
-#define BLYNK_AUTH_TOKEN "EGcX0nlkD4x9Pm9G4Zvvn_jVLK6S1t1K" 
+#define BLYNK_AUTH_TOKEN    "EGcX0nlkD4x9Pm9G4Zvvn_jVLK6S1t1K"
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <Wire.h>
 #include "DHT.h"
-#include <time.h> 
+#include "RTClib.h"
+#include <Preferences.h>
 
-char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "Testdoan"; 
-char pass[] = "tankietne123"; 
+const char* WIFI_SSID = "Testdoan";
+const char* WIFI_PASS = "tankietne123";
 
-#define RELAY_ON LOW   
-#define RELAY_OFF HIGH 
-#define DHTPIN 4
+#define RELAY_ON  HIGH
+#define RELAY_OFF LOW
+
+#define DHTPIN  4
 #define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
+
+const int PIN_VAN[3]  = {25, 26, 27};
+const int PIN_CHAU[3] = {32, 33, 35};
+const int PIN_BOM     = 14;
+
+// Giá trị ADC thực tế của cảm biến
+const int ADC_KHO = 4095;
+const int ADC_UOT = 1350;
+
+DHT      dht(DHTPIN, DHTTYPE);
+RTC_DS3231 rtc;
 BlynkTimer timer;
+Preferences prefs;
 
-const int pinChau[3] = {32, 33, 34};
-const int pinVan[3] = {25, 26, 27};
-const int pinBom = 14;
-const int mucKho = 4095, mucUot = 1350;
+float    nhietDoKK = 0, doAmKK = 0;
+int      doAmDat[3] = {0, 0, 0}; 
 
-int doAm[3]; float nhietDoKK = 0, doAmKK = 0;
-int cheDo[3] = {1, 1, 1}; 
-int nguongProfile[3] = {60, 60, 60}; 
-int gioHen = 7, phutHen = 0, doAmChung = 75, gioPhutDaTuoi = -1;     
-bool dangTuoiHenGio = false;
-bool vanHoanThanh[3] = {false, false, false};
+int      cheDo[3]   = {0, 0, 0};
+int      loaiCay[3]     = {10, 10, 10};
+int      nguongDoAm[3]  = {0,  0,  0};
 
-BLYNK_WRITE(V16) { cheDo[0] = param.asInt(); }
-BLYNK_WRITE(V17) { cheDo[1] = param.asInt(); }
-BLYNK_WRITE(V18) { cheDo[2] = param.asInt(); }
-BLYNK_WRITE(V13) { gioHen = param.asInt(); }
-BLYNK_WRITE(V14) { phutHen = param.asInt(); }
-BLYNK_WRITE(V15) { doAmChung = param.asInt(); }
-BLYNK_WRITE(V8) { nguongProfile[0] = layNguongTuoi(param.asInt()); }
-BLYNK_WRITE(V9) { nguongProfile[1] = layNguongTuoi(param.asInt()); }
-BLYNK_WRITE(V10) { nguongProfile[2] = layNguongTuoi(param.asInt()); }
-BLYNK_WRITE(V4) { if (!dangTuoiHenGio && cheDo[0] == 0) digitalWrite(pinVan[0], param.asInt() ? RELAY_ON : RELAY_OFF); }
-BLYNK_WRITE(V5) { if (!dangTuoiHenGio && cheDo[1] == 0) digitalWrite(pinVan[1], param.asInt() ? RELAY_ON : RELAY_OFF); }
-BLYNK_WRITE(V6) { if (!dangTuoiHenGio && cheDo[2] == 0) digitalWrite(pinVan[2], param.asInt() ? RELAY_ON : RELAY_OFF); }
-BLYNK_WRITE(V7) { if (!dangTuoiHenGio) digitalWrite(pinBom, param.asInt() ? RELAY_ON : RELAY_OFF); }
+int      gioHen[2]  = {7,  24};
+int      phutHen[2] = {0,  0};
+int      thoiGianTuoi = 5;       
+bool     dangTuoiHenGio = false;
+int      phutDaTuoi     = -1;
+unsigned long thoiDiemBatDau = 0;
 
-int layNguongTuoi(int loaiCay) {
-  switch (loaiCay) {
-    case 0: return 90; case 1: return 70; case 2: return 70; case 3: return 50;  
-    case 4: return 60; case 5: return 60; case 6: return 65; case 7: return 60;  
-    case 8: return 60; case 9: return 60; case 10: return 0; case 11: return 0;  
-    default: return 0; 
-  }
+bool vanState[3] = {false, false, false};
+
+inline int relayMuc(int vanIndex, bool batLen) {
+  if (vanIndex == 0) batLen = !batLen; 
+  return batLen ? RELAY_ON : RELAY_OFF;
 }
 
-void docCamBien() {
-  for (int i = 0; i < 3; i++) {
-    if (nguongProfile[i] == 0) { doAm[i] = 0; Blynk.virtualWrite(i + 1, 0); continue; }
-    int raw = analogRead(pinChau[i]);
-    doAm[i] = map(raw, mucKho, mucUot, 0, 100); doAm[i] = constrain(doAm[i], 0, 100);
-    Blynk.virtualWrite(i + 1, doAm[i]); 
+int layNguong(int loai) {
+  switch (loai) {
+    case 0: return 90;  
+    case 1: return 70;  
+    case 2: return 70;  
+    case 3: return 50;  
+    case 4: return 60;  
+    case 5: return 60;  
+    case 6: return 65;  
+    case 7: return 60;  
+    case 8: return 60;  
+    case 9: return 60;  
+    default: return 0;  
   }
-  float h = dht.readHumidity(); float t = dht.readTemperature();
-  if (!isnan(h) && !isnan(t)) { nhietDoKK = t; doAmKK = h; Blynk.virtualWrite(V11, t); Blynk.virtualWrite(V12, h); }
+}
+// ---------------------------------------------------------
+// THUẬT TOÁN BẢO VỆ PHẦN CỨNG
+// ---------------------------------------------------------
+void ngatBomVaTatVan(int i) {
+    digitalWrite(PIN_BOM, RELAY_OFF);
+    Blynk.virtualWrite(V7, 0);
+    delay(500); // Xả áp suất chống phụt nước
+    digitalWrite(PIN_VAN[i], relayMuc(i, false));
+    vanState[i] = false;
+    Blynk.virtualWrite(V4 + i, 0);
 }
 
-void xuLyLogic() {
-  struct tm timeinfo;
-  int gioHienTai = -1, phutHienTai = -1;
-  
-  if (getLocalTime(&timeinfo)) {
-    if (timeinfo.tm_year > 100) { 
-      gioHienTai = timeinfo.tm_hour; phutHienTai = timeinfo.tm_min;
+void tatVan(int i, bool buocTat = false) {
+  if (buocTat || cheDo[i] != 1) {
+    bool cacVanKhacDong = true;
+    for(int j=0; j<3; j++) {
+       if (i != j && vanState[j]) cacVanKhacDong = false;
     }
-  }
-
-  int totalHienTai = gioHienTai * 60 + phutHienTai;
-  int totalHen = gioHen * 60 + phutHen;
-
-  if (totalHienTai == totalHen && gioPhutDaTuoi != totalHienTai && gioHienTai != -1) {
-    dangTuoiHenGio = true; gioPhutDaTuoi = totalHienTai; 
-    for(int i=0; i<3; i++) vanHoanThanh[i] = false; 
-  }
-  if (totalHienTai != totalHen) gioPhutDaTuoi = -1;
-
-  bool batKyVanNaoMo = false; bool conChauCanTuoiHenGio = false;
-
-  for (int i = 0; i < 3; i++) {
-    if (nguongProfile[i] == 0) { digitalWrite(pinVan[i], RELAY_OFF); Blynk.virtualWrite(V4 + i, 0); continue; }
-
-    if (cheDo[i] == 1) {
-      int target = nguongProfile[i];
-      if (nhietDoKK > 34.0) target += 10; 
-      if (doAm[i] < (target - 5)) { digitalWrite(pinVan[i], RELAY_ON); Blynk.virtualWrite(V4 + i, 1); } 
-      else if (doAm[i] >= target) { digitalWrite(pinVan[i], RELAY_OFF); Blynk.virtualWrite(V4 + i, 0); }
+    if (cacVanKhacDong) {
+        ngatBomVaTatVan(i);
     } else {
-      if (dangTuoiHenGio) {
-        if (doAm[i] >= doAmChung) { vanHoanThanh[i] = true; digitalWrite(pinVan[i], RELAY_OFF); Blynk.virtualWrite(V4 + i, 0); } 
-        else if (!vanHoanThanh[i]) { digitalWrite(pinVan[i], RELAY_ON); Blynk.virtualWrite(V4 + i, 1); conChauCanTuoiHenGio = true; }
-      }
+        digitalWrite(PIN_VAN[i], relayMuc(i, false));
+        vanState[i] = false;
+        Blynk.virtualWrite(V4 + i, 0);
     }
-    if (digitalRead(pinVan[i]) == RELAY_ON) batKyVanNaoMo = true;
   }
-
-  if (dangTuoiHenGio && !conChauCanTuoiHenGio) dangTuoiHenGio = false;
-
-  if (batKyVanNaoMo) { digitalWrite(pinBom, RELAY_ON); Blynk.virtualWrite(V7, 1); } 
-  else { digitalWrite(pinBom, RELAY_OFF); Blynk.virtualWrite(V7, 0); }
 }
 
-BLYNK_CONNECTED() { Blynk.syncAll(); }
-
-void setup() {
-  Serial.begin(115200);
-  for (int i = 0; i < 3; i++) { digitalWrite(pinVan[i], RELAY_OFF); pinMode(pinVan[i], OUTPUT); }
-  digitalWrite(pinBom, RELAY_OFF); pinMode(pinBom, OUTPUT);
-  dht.begin(); Blynk.begin(auth, ssid, pass); 
-  configTime(7 * 3600, 0, "pool.ntp.org", "time.nist.gov");
-  timer.setInterval(2000L, docCamBien); timer.setInterval(3000L, xuLyLogic);   
+void moVan(int i) {
+  digitalWrite(PIN_VAN[i], relayMuc(i, true));
+  vanState[i] = true;
+  Blynk.virtualWrite(V4 + i, 1);
 }
-void loop() { Blynk.run(); timer.run(); }
+
+bool coVanNaoMo() {
+  for (int i = 0; i < 3; i++)
+    if (vanState[i]) return true;
+  return false;
+}
+
+bool coVanAutoMo() {
+  for (int i = 0; i < 3; i++) {
+    if (vanState[i] && (cheDo[i] == 1 || dangTuoiHenGio)) return true;
+  }
+  return false;
+}
